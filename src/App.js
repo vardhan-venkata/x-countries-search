60adbdf6488b8bf59e6138c96301360a4e60c9c2
vardhan-venkata <vardhanmerapureddy@gmail.com>
2025-05-01 09:12:10 +0530

    Implemented x coutnries search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ListCountries from "./components/ListCountries";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <input
+          type="text"
+          value={searchValue}
+          placeholder="Search for countries"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <ListCountries searchValue={searchValue} />
     </div>
   );
 }
---
+++ src/components/ListCountries.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./ListCountries.css";
+
+function ListCountries({ searchValue }) {
+  const [countriesData, setCountriesData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const fetchCountries = async () => {
+    try {
+      let response = await axios.get(
+        "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+      );
+      console.log("response", response);
+      setCountriesData(response.data);
+      setFilteredData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+  useEffect(() => {
+    if (searchValue) {
+      let fil =
+        filteredData &&
+        filteredData.length > 0 &&
+        filteredData.filter((ele) => ele.common.includes(searchValue));
+      console.log(fil);
+      setFilteredData(fil);
+    } else {
+      setFilteredData(countriesData);
+    }
+  }, [searchValue]);
+  return (
+    <div className="main">
+      <h1>Countries List</h1>
+      <div className="countryList">
+        {filteredData &&
+          filteredData.length > 0 &&
+          filteredData.map((ele) => {
+            return (
+              <div className="countryCard">
+                <img src={ele.png} alt={ele.common} className="image" />
+                <p>{ele.common}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default ListCountries;
